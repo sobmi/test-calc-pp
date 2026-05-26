9177f90eeff9471ecc0c0c8bb6f88130ce8f707d
Michał S <47273222+Xarix200@users.noreply.github.com>
2026-05-26 15:51:17 +0200

    Minor fixes

--- utils.py
+++ utils.py
@@ -1,15 +1,19 @@
 # przykladowe funkcje utils .py
 def add(a: int, b: int) -> int:
+    """Adds two numbers"""
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
+    """Substracts two numbers"""
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
+    """Multiplies two numbers"""
     return a * b
 
 
 def divide(a: int, b: int) -> float:
+    """Divides two numbers"""
     return a / b
