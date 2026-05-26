7cabdb69ded16f16b03066a28fa56b8a8c27d100
Michał S <47273222+Xarix200@users.noreply.github.com>
2026-05-26 15:54:48 +0200

    Minor fixes 2

--- utils.py
+++ utils.py
@@ -1,19 +1,32 @@
 # przykladowe funkcje utils .py
+"""
+This module contains the utility functions for the calculator app.
+"""
+
+
 def add(a: int, b: int) -> int:
-    """Adds two numbers"""
+    """
+    Adds two numbers.
+    """
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
-    """Substracts two numbers"""
+    """
+    Subtracts two numbers.
+    """
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
-    """Multiplies two numbers"""
+    """
+    Multiplies two numbers.
+    """
     return a * b
 
 
 def divide(a: int, b: int) -> float:
-    """Divides two numbers"""
+    """
+    Divides two numbers.
+    """
     return a / b
